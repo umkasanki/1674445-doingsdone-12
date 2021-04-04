22c52eea593f5dc35229cebc5efcff7e5a7fa37e
Oleg Tishkin <umkasanki@gmail.com>
2021-04-04 11:25:05 +0300

    [search] Add search form params

--- templates/main.php
+++ templates/main.php
@@ -4,8 +4,8 @@
     <main class="content__main">
         <h2 class="content__main-heading">Список задач</h2>
 
-        <form class="search-form" action="index.php" method="post" autocomplete="off">
-            <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
+        <form class="search-form" action="index.php" method="get" autocomplete="off">
+            <input class="search-form__input" type="text" name="q" value="" placeholder="Поиск по задачам">
 
             <input class="search-form__submit" type="submit" name="" value="Искать">
         </form>
