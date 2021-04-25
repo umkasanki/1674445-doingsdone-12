dfa03bfa8ea6c866a7012d2cb5430fd2618aef6f
Oleg Tishkin <umkasanki@gmail.com>
2021-04-25 13:56:35 +0300

    [notify] add static data test

--- helpers.php
+++ helpers.php
@@ -146,3 +146,63 @@ function include_template($name, array $data = []) {
 function getPostVal($name) {
     return $_POST[$name] ?? "";
 }
+
+/**
+ * Pretty var_dump
+ * Possibility to set a title, a background-color and a text color
+ */
+function dump($data, $title="", $background="#EEEEEE", $color="#000000"){
+
+    //=== Style
+    echo "
+    <style>
+        /* Styling pre tag */
+        pre {
+            padding:10px 20px;
+            white-space: pre-wrap;
+            white-space: -moz-pre-wrap;
+            white-space: -pre-wrap;
+            white-space: -o-pre-wrap;
+            word-wrap: break-word;
+        }
+
+        /* ===========================
+        == To use with XDEBUG
+        =========================== */
+        /* Source file */
+        pre small:nth-child(1) {
+            font-weight: bold;
+            font-size: 14px;
+            color: #CC0000;
+        }
+        pre small:nth-child(1)::after {
+            content: '';
+            position: relative;
+            width: 100%;
+            height: 20px;
+            left: 0;
+            display: block;
+            clear: both;
+        }
+
+        /* Separator */
+        pre i::after{
+            content: '';
+            position: relative;
+            width: 100%;
+            height: 15px;
+            left: 0;
+            display: block;
+            clear: both;
+            border-bottom: 1px solid grey;
+        }
+    </style>
+    ";
+
+    //=== Content
+    echo "<pre style='background:$background; color:$color; padding:10px 20px; border:2px inset $color'>";
+    echo    "<h2>$title</h2>";
+    var_dump($data);
+    echo "</pre>";
+
+}
--- notify.php
+++ notify.php
@@ -1,45 +1,178 @@
 <?php
 require_once 'vendor/autoload.php';
 require_once 'DbHelper.php';
+require_once 'helpers.php';
 
 $dbHelper = new DbHelper("mysql", "mysql", "127.0.0.1", "doit");
 
 if (!$dbHelper->getLastError()) {
-    $tasks = mysqli_fetch_all($dbHelper->executeQuery("SELECT * FROM tasks WHERE status = 0 AND expire_date = CURRENT_DATE()"));
+    $tasks = mysqli_fetch_all($dbHelper->executeQuery(
+        "SELECT u.id, u.email, u.name, t.name, t.user_id, t.expire_date, t.id as task_id
+            FROM users u
+            JOIN tasks t
+            ON u.id = t.user_id
+            WHERE t.status = 0
+            AND expire_date = CURRENT_DATE();"));
 }
 else {
     print($dbHelper->getLastError());
 }
 
-if (count($tasks)) {
-    try {
-        // Create the Transport
-        $transport = (new Swift_SmtpTransport('phpdemo.ru', 25))
-            ->setUsername('keks@phpdemo.ru')
-            ->setPassword('htmlacademy')
-        ;
-
-        // Create the Mailer using your created Transport
-        $mailer = new Swift_Mailer($transport);
-
-        // Create a message
-        $message = new Swift_Message();
-        $message->setSubject('Уведомление от сервиса «Дела в порядке');
-        $message->setFrom(['keks@phpdemo.ru']);
-        $message->addTo('umkasanki@gmail.com');
-
-        $messageText = '<b>Сегодня нужно выполнить следующие задачи:</b><br>';
-
-        foreach ($tasks as $task) {
-            $messageText .= ('- ' . $task[3] . '<br>');
-        }
-        $message->setBody($messageText, 'text/html');
-
-        // Send the message
-        $result = $mailer->send($message);
-    } catch (Exception $e) {
-        echo $e->getMessage();
-    }
+if (!$dbHelper->getLastError()) {
+    $tasksByUser = mysqli_fetch_all($dbHelper->executeQuery(
+        'SELECT u.id, u.email, u.name, t.name, t.user_id, t.expire_date, t.id as task_id
+            FROM users u
+            JOIN tasks t
+            ON u.id = t.user_id
+            WHERE t.status = 0
+            AND expire_date = CURRENT_DATE()'
+    ));
 }
+else {
+    print($dbHelper->getLastError());
+}
+
+$notificationData = array();
+
+// test
+$notificationDataSchema = array(
+    'oleg' => array(
+        'name' => 'Oleg',
+        'email' => 'umkasanki@gmail.com',
+        'tasks' => array(
+            array(
+                'task name',
+                'task date',
+            ),
+            array(
+                'task 2 name',
+                'task 2 date',
+            ),
+        )
+    ),
+    'denis' => array(
+        'name' => 'Denis',
+        'email' => 'denis@tishkin.com',
+        'tasks' => array(
+            array(
+                'task 3 name',
+                'task 3 date',
+            ),
+            array(
+                'task 5 name',
+                'task 5 date',
+            ),
+        )
+    ),
+);
+
+$userArr = array(
+    'roma' => array(
+        'name' => 'Roma',
+        'email' => 'roma@tishkin.com',
+        'tasks' => array(
+            array(
+                'task 3 name',
+                'task 3 date',
+            ),
+            array(
+                'task 5 name',
+                'task 5 date',
+            ),
+        )
+    ),
+);
+
+$notificationDataSchema = array_merge($notificationDataSchema, array(
+    'roma' => array(
+        'name' => 'Roma',
+        'email' => 'roma@tishkin.com',
+        'tasks' => array(
+            array(
+                'task 3 name',
+                'task 3 date',
+            ),
+            array(
+                'task 5 name',
+                'task 5 date',
+            ),
+        )
+    ),
+));
+
+dump($notificationDataSchema);
+print '<br>';
+
+foreach ($notificationDataSchema as $key => $value) {
+    print '<br>';
+    print $key;
+    print '<hr>';
+
+//    if (array_key_exists($key, $notificationDataSchema)) {
+//        print ('найдено ' . $key);
+//    } else {
+//        print ('не найдено ' . $key);
+//    }
+    print '<br>';
+}
+// test
+
+
+
+//print '$tasksByUser <hr>';
+//var_dump($tasksByUser);
+
+
+//var_dump($notificationData);
+
+//foreach ($notificationData as $key => $value) {
+//    var_dump($key);
+//    var_dump($notificationData[$key]['email']);
+////    var_dump($notificationData[$key]['tasks']);
+////    var_dump($notificationData[$key]);
+////    print $notificationData[$key]['email'];
+//    print '<br>';
+//    print '<br>';
+//
+//    foreach ($notificationData[$key]['tasks'] as $item) {
+//        var_dump($item);
+////        print '<br>';
+//    }
+//    print '<br>';
+//    print '<hr>';
+//
+//}
+
+//if (count($notificationData)) {
+//    // Create the Transport
+//    $transport = (new Swift_SmtpTransport('phpdemo.ru', 25))
+//        ->setUsername('keks@phpdemo.ru')
+//        ->setPassword('htmlacademy')
+//    ;
+//    // Create the Mailer using your created Transport
+//    $mailer = new Swift_Mailer($transport);
+//
+//    foreach ($notificationDataSchema as $key => $value) {
+//        try {
+//            // Create a message
+//            $message = new Swift_Message();
+//            $message->setSubject('Уведомление от сервиса «Дела в порядке');
+//            $message->setFrom(['keks@phpdemo.ru']);
+//            $message->addTo($notificationDataSchema[$key]['email']);
+//
+//            $messageText = '<b>Уважаемый, ' . $key . ' следующие задачи:</b><br>';
+//
+//            foreach ($notificationData[$userName]['tasks'] as $task) {
+//                $messageText .= ('- У вас запланирована задача' . $task[0] . ' на  ' . $task[1] . '<br>');
+//            }
+//            $message->setBody($messageText, 'text/html');
+//
+//            // Send the message
+////            $result = $mailer->send($message);
+//        } catch (Exception $e) {
+//            echo $e->getMessage();
+//        }
+//    }
+//}
 
 ?>
