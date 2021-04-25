f5b8c871b213c9bb8275233b2a0971e3a0c6f8d4
Oleg Tishkin <umkasanki@gmail.com>
2021-04-25 19:06:51 +0300

    [notify] Hide debug code

--- notify.php
+++ notify.php
@@ -36,90 +36,89 @@ else {
 
 $notificationData = array();
 
-// test
-print '<h2>$tasksData</h2>';
-dump($tasksData);
-
-$notificationDataSchema = array(
-    'oleg' => array(
-        'name' => 'Oleg',
-        'email' => 'umkasanki@gmail.com',
-        'tasks' => array(
-            array(
-                'task name',
-                'task date',
-            ),
-            array(
-                'task 2 name',
-                'task 2 date',
-            ),
-        )
-    ),
-    'denis' => array(
-        'name' => 'Denis',
-        'email' => 'denis@tishkin.com',
-        'tasks' => array(
-            array(
-                'task 3 name',
-                'task 3 date',
-            ),
-            array(
-                'task 5 name',
-                'task 5 date',
-            ),
-        )
-    ),
-);
-
-$userArr = array(
-    ('user' . '3') => array(
-        'name' => 'Roma',
-        'email' => 'roma@tishkin.com',
-        'tasks' => array(
-            array(
-                'task 3 name',
-                'task 3 date',
-            ),
-            array(
-                'task 5 name',
-                'task 5 date',
-            ),
-        )
-    ),
-);
-
-$notificationDataSchema = array_merge($notificationDataSchema, array(
-    ('user' . '3') => array(
-        'name' => 'Roma',
-        'email' => 'roma@tishkin.com',
-        'tasks' => array(
-            array(
-                'task 3 name',
-                'task 3 date',
-            ),
-            array(
-                'task 5 name',
-                'task 5 date',
-            ),
-        )
-    ),
-));
-
-print '<h2>$notificationDataSchema</h2>';
-dump($notificationDataSchema);
-
-foreach ($notificationDataSchema as $key => $value) {
-    print '<h3>new email</h3>';
-    print '<div>' . $value['name'] . '</div>';
-    print '<div>' . $value['email'] . '</div>';
-
-    foreach ($value['tasks'] as $task) {
-        print '<li>' . $task[0] . '</li>';
-        print '<li>' . $task[1] . '</li>';
-        print '<br>';
-    }
-    print '<hr>';
-}
+//print '<h2>$tasksData</h2>';
+//dump($tasksData);
+//
+//$notificationDataSchema = array(
+//    'oleg' => array(
+//        'name' => 'Oleg',
+//        'email' => 'umkasanki@gmail.com',
+//        'tasks' => array(
+//            array(
+//                'task name',
+//                'task date',
+//            ),
+//            array(
+//                'task 2 name',
+//                'task 2 date',
+//            ),
+//        )
+//    ),
+//    'denis' => array(
+//        'name' => 'Denis',
+//        'email' => 'denis@tishkin.com',
+//        'tasks' => array(
+//            array(
+//                'task 3 name',
+//                'task 3 date',
+//            ),
+//            array(
+//                'task 5 name',
+//                'task 5 date',
+//            ),
+//        )
+//    ),
+//);
+//
+//$userArr = array(
+//    ('user' . '3') => array(
+//        'name' => 'Roma',
+//        'email' => 'roma@tishkin.com',
+//        'tasks' => array(
+//            array(
+//                'task 3 name',
+//                'task 3 date',
+//            ),
+//            array(
+//                'task 5 name',
+//                'task 5 date',
+//            ),
+//        )
+//    ),
+//);
+//
+//$notificationDataSchema = array_merge($notificationDataSchema, array(
+//    ('user' . '3') => array(
+//        'name' => 'Roma',
+//        'email' => 'roma@tishkin.com',
+//        'tasks' => array(
+//            array(
+//                'task 3 name',
+//                'task 3 date',
+//            ),
+//            array(
+//                'task 5 name',
+//                'task 5 date',
+//            ),
+//        )
+//    ),
+//));
+//
+//print '<h2>$notificationDataSchema</h2>';
+//dump($notificationDataSchema);
+//
+//foreach ($notificationDataSchema as $key => $value) {
+//    print '<h3>new email</h3>';
+//    print '<div>' . $value['name'] . '</div>';
+//    print '<div>' . $value['email'] . '</div>';
+//
+//    foreach ($value['tasks'] as $task) {
+//        print '<li>' . $task[0] . '</li>';
+//        print '<li>' . $task[1] . '</li>';
+//        print '<br>';
+//    }
+//    print '<hr>';
+//}
 
 //dump($tasksData);
 
@@ -129,33 +128,32 @@ foreach ($tasksData as $taskLine) {
         $notificationData = array_merge($notificationData, array(
             $userHandler => array(),
         ));
-        $notificationData[$userHandler]['tasks'] = array();
+        $notificationData[$userHandler]['email'] = $taskLine[1];
+        $notificationData[$userHandler]['name'] = $taskLine[2];
     }
-    $notificationData[$userHandler]['email'] = $taskLine[1];
-    $notificationData[$userHandler]['name'] = $taskLine[2];
-
-    $notificationData[$userHandler]['tasks'] = array_merge($notificationData[$userHandler]['tasks'], array(
+    $notificationData[$userHandler]['tasks'][] = array(
         $taskLine[3],
         $taskLine[5]
-    ));
+    );
 }
 
-print '<h2>$notificationData</h2>';
-dump($notificationData);
+//print '<h2>$notificationData</h2>';
+//dump($notificationData);
 
-foreach ($notificationData as $userData) {
-    print '<br>';
-    print '<h3>new email</h3>';
-    print '<div>' . $userData['name'] . '</div>';
-    print '<div>' . $userData['email'] . '</div>';
-
-    foreach ($userData['tasks'] as $task) {
-        print '<li>' . $task[0] . '</li>';
-        print '<li>' . $task[1] . '</li>';
-        print '<br>';
-    }
-    print '<hr>';
-}
+//foreach ($notificationData as $userData) {
+//    print '<br>';
+//    print '<h3>new email</h3>';
+//    print '<div>' . $userData['name'] . '</div>';
+//    print '<div>' . $userData['email'] . '</div>';
+//
+//    foreach ($userData['tasks'] as $task) {
+//        dump($task);
+//        print '<li>' . $task[0] . '</li>';
+//        print '<li>' . $task[1] . '</li>';
+//        print '<br>';
+//    }
+//    print '<hr>';
+//}
 
 if (count($notificationData)) {
     // Create the Transport
