4d44e7d82b82c9c79bb99b08b5007d4f45f0c9b4
Oleg Tishkin <umkasanki@gmail.com>
2021-05-16 12:07:26 +0300

    [notify.php] remove comments

--- notify.php
+++ notify.php
@@ -36,92 +36,6 @@ else {
 
 $notificationData = array();
 
-//print '<h2>$tasksData</h2>';
-//dump($tasksData);
-//
-//$notificationDataSchema = array(
-//    'oleg' => array(
-//        'name' => 'Oleg',
-//        'email' => 'umkasanki@gmail.com',
-//        'tasks' => array(
-//            array(
-//                'task name',
-//                'task date',
-//            ),
-//            array(
-//                'task 2 name',
-//                'task 2 date',
-//            ),
-//        )
-//    ),
-//    'denis' => array(
-//        'name' => 'Denis',
-//        'email' => 'denis@tishkin.com',
-//        'tasks' => array(
-//            array(
-//                'task 3 name',
-//                'task 3 date',
-//            ),
-//            array(
-//                'task 5 name',
-//                'task 5 date',
-//            ),
-//        )
-//    ),
-//);
-//
-//$userArr = array(
-//    ('user' . '3') => array(
-//        'name' => 'Roma',
-//        'email' => 'roma@tishkin.com',
-//        'tasks' => array(
-//            array(
-//                'task 3 name',
-//                'task 3 date',
-//            ),
-//            array(
-//                'task 5 name',
-//                'task 5 date',
-//            ),
-//        )
-//    ),
-//);
-//
-//$notificationDataSchema = array_merge($notificationDataSchema, array(
-//    ('user' . '3') => array(
-//        'name' => 'Roma',
-//        'email' => 'roma@tishkin.com',
-//        'tasks' => array(
-//            array(
-//                'task 3 name',
-//                'task 3 date',
-//            ),
-//            array(
-//                'task 5 name',
-//                'task 5 date',
-//            ),
-//        )
-//    ),
-//));
-//
-//print '<h2>$notificationDataSchema</h2>';
-//dump($notificationDataSchema);
-//
-//foreach ($notificationDataSchema as $key => $value) {
-//    print '<h3>new email</h3>';
-//    print '<div>' . $value['name'] . '</div>';
-//    print '<div>' . $value['email'] . '</div>';
-//
-//    foreach ($value['tasks'] as $task) {
-//        print '<li>' . $task[0] . '</li>';
-//        print '<li>' . $task[1] . '</li>';
-//        print '<br>';
-//    }
-//    print '<hr>';
-//}
-
-//dump($tasksData);
-
 foreach ($tasksData as $taskLine) {
     $userHandler = 'user' . $taskLine[0];
     if (!array_key_exists($userHandler, $tasksData)) {
@@ -136,24 +50,6 @@ foreach ($tasksData as $taskLine) {
         $taskLine[5]
     );
 }
-
-//print '<h2>$notificationData</h2>';
-//dump($notificationData);
-
-//foreach ($notificationData as $userData) {
-//    print '<br>';
-//    print '<h3>new email</h3>';
-//    print '<div>' . $userData['name'] . '</div>';
-//    print '<div>' . $userData['email'] . '</div>';
-//
-//    foreach ($userData['tasks'] as $task) {
-//        dump($task);
-//        print '<li>' . $task[0] . '</li>';
-//        print '<li>' . $task[1] . '</li>';
-//        print '<br>';
-//    }
-//    print '<hr>';
-//}
 
 if (count($notificationData)) {
     // Create the Transport
