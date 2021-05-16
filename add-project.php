d05036ba48b6f110f0bf1b856f21fc30582eb991
Oleg Tishkin <umkasanki@gmail.com>
2021-05-16 13:07:00 +0300

    [add projec] add category existence check

--- add-project.php
+++ add-project.php
@@ -30,45 +30,13 @@ $tasks_list = mysqli_fetch_all($get_tasks_res, MYSQLI_ASSOC);
 // db queries end
 
 // обработка формы
-if (isset($_FILES['file'])) {
-    $file_name = $_FILES['file']['name'];
-    $uploadPath = __DIR__ . '/uploads/';
-    $fileUrl = '/uploads/' . $file_name;
-    move_uploaded_file($_FILES['file']['tmp_name'], $uploadPath . $file_name);
-}
-
-function getFilesVal($name) {
-    if (isset($_FILES[$name])) {
-        $file_name = $_FILES[$name]['name'];
-        $fileUrl = '/uploads/' . $file_name;
-        return compact('file_name', 'fileUrl');
-    }
-    // @todo вопрос: нужен ли тут return?
-}
-
-function validateFilled($name) {
-    if (empty($_POST[$name])) {
-        return "Это поле должно быть заполнено";
-    }
-}
-
-function validateEmail($name) {
-    if (!filter_input(INPUT_POST, $name, FILTER_VALIDATE_EMAIL)) {
-        return "Введите корректный email";
-    }
-}
-
-// @todo вопрос: Для идентификатора выбранного проекта проверять, что он ссылается на реально существующий проект.
-//function validateCategory() {
-//    foreach ($tasks_categories as $value) {
-//        if ($value['cat_id'] == $_POST['project']) {
-//            return true;
-//        }
-//    }
-//    return 'Выберите проект';
-//}
 
 $errors = [];
+foreach ($tasks_categories as $category) {
+    if ($category['cat_name'] == get_post_val('name')) {
+        $errors['name'] = 'Название проекта занято';
+    }
+}
 foreach ($_POST as $key => $value) {
     if ($key == 'name') {
         $len = strlen($_POST[$key]);
@@ -82,20 +50,19 @@ $errors = array_filter($errors);
 
 // отправка запросов
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && count($errors) === 0) {
-    $addTaskQr = "INSERT INTO `categories` (cat_name, user_id)
+    $add_category_query = "INSERT INTO `categories` (cat_name, user_id)
                   VALUES (?, ?)";
 
-    $stmp = mysqli_prepare($conn, $addTaskQr);
+    $stmp = mysqli_prepare($conn, $add_category_query);
 
     $name = get_post_val('name');
-    $user_id = $user_id;
 
     mysqli_stmt_bind_param($stmp, 'si',
         $name, $user_id);
 
-    $addTaskQrResult = mysqli_stmt_execute($stmp);
+    $add_category_query_result = mysqli_stmt_execute($stmp);
 
-    if (!$addTaskQrResult) {
+    if (!$add_category_query_result) {
         $error = mysqli_error($conn);
         print("Ошибка MySQL: " . $error);
     }
